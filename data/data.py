c6ba2d9387536b25927e7096d6a782720065aff7
Saul Zepeda <saul.zepeda@toshibagcs.com>
2022-05-27 20:09:55 -0500

    updates may 26

--- data/data.py
+++ data/data.py
@@ -2,7 +2,43 @@ import csv
 import json
 import re
 
+#from importlib_metadata import distribution
+tens = dict(k=10e3, m=10e6, b=10e9)
 regions_data = dict()
+class Region:
+    def __init__(self, name):
+        self.name = name
+        self.territory = Variable('territory')
+        self.population = Variable('population')
+        self.growth_rate = Variable('growth_rate')
+        self.income = Variable('income')
+        self.literacy_rate = Variable('literacy_rate')
+        self.military_spdng = Variable('military_spdng')
+class Variable:
+    def __init__(self, name):
+        self.name = name
+        self.data = []
+        self.distribution = ''
+        self.mean = 0
+        self.stdv = 0
+
+def conversion(x):
+    factor, exp = x[0:-1], x[-1].lower()
+    ans = int(float(factor) * tens[exp])
+    return ans
+
+regions = dict()
+
+regions['europe'] = Region('Europe')
+regions['africa'] = Region('Africa')
+regions['asia'] = Region('Asia')
+regions['n_america'] = Region('N America')
+regions['s_america'] = Region('S America')
+regions['oceania'] = Region('Oceania')
+regions['seme_asia'] = Region('S E Asia M E')
+
+
+
 
 with open('src/world_region.csv', mode='r', encoding="utf8") as inp:
     reader = csv.reader(inp)
@@ -16,8 +52,32 @@ with open('src/world_region.csv', mode='r', encoding="utf8") as inp:
             regions_data[region].add(country)
         
 
-print(regions_data)
-print(len(regions_data))
+with open('src/population_total.csv', mode='r', encoding="utf8") as pop:
+    reader = csv.reader(pop)
+    next(reader, None)
+    for row in reader:
+        #print(row)
+        country = row[0]
+        for continent, countries in regions_data.items():
+            if country in countries:
+                if continent == 'europe':
+                    new_row = row[1:]
+                    converted = [conversion(x) for x in new_row]
+                    
+                    if not regions[continent].population.data:
+                        regions[continent].population.data = converted
+                    else: 
+                    
+                        for index, elem in enumerate(regions[continent].population.data):
+                            regions[continent].population.data[index] += converted[index]
+                    print(f"agrega {new_row[20]} : {regions[continent].population.data[20]}")
+
+
+
+'''print(regions_data)
+print(len(regions_data))'''
+
+#seme_asia.population.data = 
 
 
 '''
